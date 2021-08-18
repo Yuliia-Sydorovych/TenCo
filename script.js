e5765082431c74ec12a1b2df0072a387fa17b683
Yuliia-Sydorovych <yuliiasydorovych@gmail.com>
2021-08-18 12:20:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function ChoseHome()
+{
+    document.getElementById("home").style.display = "block";
+    document.getElementById("services").style.display = "none";
+    document.getElementById("contactUs").style.display = "none";
+}
+function ChoseServices()
+{
+    document.getElementById("services").style.display = "block";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("contactUs").style.display = "none";
+}
+function ChoseContact()
+{
+    document.getElementById("contactUs").style.display = "block";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("services").style.display = "none";
+}
